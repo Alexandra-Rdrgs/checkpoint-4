cb7c5e4966dd74564cbfe0c195ae13171e3c69c0
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-20 11:30:10 +0200

    Add crud for kingdoms in router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -15,4 +15,11 @@ router.get("/wealth/:id", ItemController.read);
 router.put("/wealth/:id", ItemController.edit);
 router.post("/wealth", ItemController.add);
 router.delete("/wealth/:id", ItemController.delete);
+
+router.get("/kingdoms", ItemController.browse);
+router.get("/kingdoms/:id", ItemController.read);
+router.put("/kingdoms/:id", ItemController.edit);
+router.post("/kingdoms", ItemController.add);
+router.delete("/kingdoms/:id", ItemController.delete);
+
 module.exports = router;
