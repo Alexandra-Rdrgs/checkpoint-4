e4379a6f987f3e3608b9c5ad6c552428f2757eda
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-20 15:33:37 +0200

    Call with axios the kingdom table and display the kingdoms infos

--- frontend/src/pages/Kingdom.jsx
+++ frontend/src/pages/Kingdom.jsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../components/Header";
 
 export default function Kingdom() {
+  const [kingdoms, setKingdoms] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `${
+          import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"
+        }/kingdoms`
+      )
+      .then((res) => {
+        setKingdoms(res.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
-      <h1>Kingdom</h1>
+      <h1>Kingdoms</h1>
+      <ul>
+        {kingdoms.map((kingdom) => (
+          <li key={kingdom.id}>
+            <img src={kingdom.image} alt={kingdom.name} />
+            <h2>{kingdom.name}</h2>
+            <p>{kingdom.population}</p>
+            <p>{kingdom.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
