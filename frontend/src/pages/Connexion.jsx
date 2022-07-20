e085acddd6672e49b4b2c7b9f570324bd455afab
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-20 14:43:52 +0200

    Import Header in Connexion.jsx

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -1,1 +1,10 @@
-export default function Connexion() {}
+import Header from "../components/Header";
+
+export default function Connexion() {
+  return (
+    <div>
+      <Header />
+      <h1>About Dralanis Kingdom</h1>
+    </div>
+  );
+}
