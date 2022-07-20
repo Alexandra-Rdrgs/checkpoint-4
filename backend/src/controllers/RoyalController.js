83739a261928f9271c1ade8d9c75c480251f2316
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-20 12:12:10 +0200

    Add RoyalController file to controllers folder

---
+++ backend/src/controllers/RoyalController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class RoyalController {
+  static browse = (req, res) => {
+    models.royal
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.royal
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const royal = req.body;
+
+    // TODO validations (length, format...)
+
+    royal.id = parseInt(req.params.id, 10);
+
+    models.royal
+      .update(royal)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const royal = req.body;
+
+    // TODO validations (length, format...)
+
+    models.royal
+      .insert(royal)
+      .then(([result]) => {
+        res.status(201).send({ ...royal, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.royal
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = RoyalController;
