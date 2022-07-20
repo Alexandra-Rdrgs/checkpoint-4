9cc18f99381c3b2da1099f7c23dd313ab8d5a1f4
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-20 11:28:35 +0200

    Add crud for wealth in router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,4 +10,9 @@ router.put("/royals/:id", ItemController.edit);
 router.post("/royals", ItemController.add);
 router.delete("/royals/:id", ItemController.delete);
 
+router.get("/wealth", ItemController.browse);
+router.get("/wealth/:id", ItemController.read);
+router.put("/wealth/:id", ItemController.edit);
+router.post("/wealth", ItemController.add);
+router.delete("/wealth/:id", ItemController.delete);
 module.exports = router;
