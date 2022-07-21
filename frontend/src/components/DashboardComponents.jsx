78fcbf14980e49208ad52f21ccd7a095694ce33b
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-21 09:26:13 +0200

    First version of the structure of the component

--- frontend/src/components/DashboardComponents.jsx
+++ frontend/src/components/DashboardComponents.jsx
@@ -1,7 +1,16 @@
-export default function DashboardComponents() {
+export default function DashboardComponents(
+  title,
+  description,
+  action,
+  actionText
+) {
   return (
     <div>
-      <h1>DashboardComponents</h1>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <button type="button" onClick={action}>
+        {actionText}
+      </button>
     </div>
   );
 }
