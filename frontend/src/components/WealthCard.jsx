8aef69d3e21351a4528e6564d4141267df790a1b
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-21 11:24:06 +0200

    Create card component to display the wealth

---
+++ frontend/src/components/WealthCard.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export default function WealthCard({ wealth }) {
+  return (
+    <article>
+      <img src={wealth.image} alt={wealth.name} />
+      <h2>{wealth.name}</h2>
+      <p>{wealth.value}</p>
+      <p>{wealth.description}</p>
+    </article>
+  );
+}
+
+WealthCard.propTypes = {
+  wealth: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
