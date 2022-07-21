e769bef6e667427ff487a7718b1703eae3d66d6f
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-21 09:57:48 +0200

    Create the structure of the form

--- frontend/src/pages/UpdateRoyals.jsx
+++ frontend/src/pages/UpdateRoyals.jsx
@@ -4,7 +4,49 @@ export default function UpdateRoyals() {
   return (
     <div>
       <Header />
-      <h1>Welcome to the Dralyanis Kingdom.</h1>
+      <h1>Update your royal family</h1>
+      <form>
+        <label htmlFor="firstname">
+          Royal firstname :
+          <input type="text" />
+        </label>
+        <label htmlFor="lastname">
+          Royal lastname :
+          <input type="text" />
+        </label>
+        <label htmlFor="mother">
+          Royal mother :
+          <input type="text" />
+        </label>
+        <label htmlFor="father">
+          Royal father :
+          <input type="text" />
+        </label>
+        <div>
+          <label htmlFor="married">
+            Married :
+            <label htmlFor="yes">
+              Yes <input type="checkbox" />
+            </label>
+            <label htmlFor="no">
+              No <input type="checkbox" />
+            </label>
+          </label>
+        </div>
+        <label htmlFor="marriedTo">
+          If married, enter the name of the spouse :
+          <input type="text" />
+        </label>
+        <label htmlFor="biography">
+          Royal biography :
+          <textarea />
+        </label>
+        <label htmlFor="image">
+          Royal picture :
+          <input type="file" />
+        </label>
+        <button type="submit">Update royal</button>
+      </form>
     </div>
   );
 }
