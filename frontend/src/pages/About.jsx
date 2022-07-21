a30c644d2a1fee1a02c1c892699ce2f20b24ba27
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-21 16:48:42 +0200

    Add style and some images

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,24 @@
+import oldCards from "../assets/oldCards.jpeg";
 import Header from "../components/Header";
 
 export default function About() {
   return (
     <div>
       <Header />
-      <h1>About Dralanis Kingdom</h1>
+      <article className="m-[2rem]">
+        <h1 className="text-xl mb-[2rem]">About Dralanis Kingdom</h1>
+        <img src={oldCards} alt="Dralanis Kingdom" className="mb-[2rem]" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde alias
+          quos nulla modi mollitia dolore temporibus, possimus voluptatem
+          tenetur iusto corrupti, facilis veniam quae amet cumque! Totam quidem
+          ad magni voluptates. Blanditiis tempore excepturi, natus magni, nam
+          obcaecati nobis consectetur doloremque laudantium amet inventore, quod
+          repudiandae quasi minus officia repellendus! Assumenda iure labore
+          exercitationem excepturi ducimus sapiente, recusandae molestias
+          obcaecati.
+        </p>
+      </article>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -5,13 +6,16 @@ export default function Home() {
     <div>
       <Header />
       <section className="m-[2rem]">
-        <h1 className="text-8xl mb-12 leading-[6.5rem]">
+        <h1 className="text-5xl mb-12">
           Welcome to the <strong>United Kingdoms of Dralyanis.</strong>
         </h1>
-        <p className="text-7xl mb-12 leading-[5.5rem]">
+        <p className="text-4xl mb-12">
           Enter and learn a little bit about <strong>our kingdoms</strong> and
           theirs <strong>royals</strong>.
         </p>
+        <Link to="/about" className="btn btn-primary">
+          Learn More →
+        </Link>
       </section>
     </div>
   );
