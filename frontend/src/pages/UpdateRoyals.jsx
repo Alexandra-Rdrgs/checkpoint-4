8f6ad14a92d7394655a09e3a8265bb6b87f141d5
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-21 10:04:31 +0200

    Final structure of the page, now lets make it work

--- frontend/src/pages/UpdateRoyals.jsx
+++ frontend/src/pages/UpdateRoyals.jsx
@@ -5,48 +5,101 @@ export default function UpdateRoyals() {
     <div>
       <Header />
       <h1>Update your royal family</h1>
-      <form>
-        <label htmlFor="firstname">
-          Royal firstname :
-          <input type="text" />
-        </label>
-        <label htmlFor="lastname">
-          Royal lastname :
-          <input type="text" />
-        </label>
-        <label htmlFor="mother">
-          Royal mother :
-          <input type="text" />
-        </label>
-        <label htmlFor="father">
-          Royal father :
-          <input type="text" />
-        </label>
-        <div>
-          <label htmlFor="married">
-            Married :
-            <label htmlFor="yes">
-              Yes <input type="checkbox" />
-            </label>
-            <label htmlFor="no">
-              No <input type="checkbox" />
-            </label>
+      <p>
+        You can keep track of your kingdom's royal population by adding or
+        updating their information here. Do you want to add a new royal family
+        or update an existing one?
+      </p>
+      <fieldset>
+        <legend>Add a new royal</legend>
+        <form>
+          <label htmlFor="firstname">
+            Royal firstname :
+            <input type="text" />
           </label>
-        </div>
-        <label htmlFor="marriedTo">
-          If married, enter the name of the spouse :
-          <input type="text" />
-        </label>
-        <label htmlFor="biography">
-          Royal biography :
-          <textarea />
-        </label>
-        <label htmlFor="image">
-          Royal picture :
-          <input type="file" />
-        </label>
-        <button type="submit">Update royal</button>
-      </form>
+          <label htmlFor="lastname">
+            Royal lastname :
+            <input type="text" />
+          </label>
+          <label htmlFor="mother">
+            Royal mother :
+            <input type="text" />
+          </label>
+          <label htmlFor="father">
+            Royal father :
+            <input type="text" />
+          </label>
+          <div>
+            <label htmlFor="married">
+              Married :
+              <label htmlFor="yes">
+                Yes <input type="checkbox" />
+              </label>
+              <label htmlFor="no">
+                No <input type="checkbox" />
+              </label>
+            </label>
+          </div>
+          <label htmlFor="marriedTo">
+            If married, enter the name of the spouse :
+            <input type="text" />
+          </label>
+          <label htmlFor="biography">
+            Royal biography :
+            <textarea />
+          </label>
+          <label htmlFor="image">
+            Royal picture :
+            <input type="file" />
+          </label>
+          <button type="submit">Add a new royal member</button>
+        </form>
+      </fieldset>
+      <fieldset>
+        <legend>Update a royal</legend>
+        <form>
+          <label htmlFor="firstname">
+            Royal firstname :
+            <input type="text" />
+          </label>
+          <label htmlFor="lastname">
+            Royal lastname :
+            <input type="text" />
+          </label>
+          <label htmlFor="mother">
+            Royal mother :
+            <input type="text" />
+          </label>
+          <label htmlFor="father">
+            Royal father :
+            <input type="text" />
+          </label>
+          <div>
+            <label htmlFor="married">
+              Married :
+              <label htmlFor="yes">
+                Yes <input type="checkbox" />
+              </label>
+              <label htmlFor="no">
+                No <input type="checkbox" />
+              </label>
+            </label>
+          </div>
+          <label htmlFor="marriedTo">
+            If married, enter the name of the spouse :
+            <input type="text" />
+          </label>
+          <label htmlFor="biography">
+            Royal biography :
+            <textarea />
+          </label>
+          <label htmlFor="image">
+            Royal picture :
+            <input type="file" />
+          </label>
+          <button type="submit">Update a royal member</button>
+        </form>
+      </fieldset>
     </div>
   );
 }
