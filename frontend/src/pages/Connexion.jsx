d67a27c09a03743b46093c5f77ad5007c95f9ef4
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-26 16:14:18 +0200

    Add style to the page to harmonize the style

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -4,18 +4,35 @@ export default function Connexion() {
   return (
     <div>
       <Header />
-      <h1>Connexion</h1>
-      <form>
-        <label htmlFor="id">
-          Id :
-          <input type="text" name="email" />
-        </label>
-        <label htmlFor="password">
-          Password :
-          <input type="text" name="password" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <section className="flex flex-col justify-center items-center m-[2rem]">
+        <h1 className="text-xl">Connexion</h1>
+        <fieldset className="mt-2 mb-8 border-2 border-gray-600 p-[2rem] max-w-[600px]">
+          <legend className="text-xl"> Log In </legend>
+          <form className="flex flex-col gap-y-[3rem]">
+            <label htmlFor="id">
+              Enter your ID here : <br />
+              <input
+                type="text"
+                name="email"
+                className="w-[300px] border-[1px] border-gray-900 mt-[1rem]"
+              />
+            </label>
+            <label htmlFor="password">
+              Enter your password here : <br />
+              <input
+                type="text"
+                name="password"
+                className="w-[300px] border-[1px] border-gray-900 mt-[1rem]"
+              />
+            </label>
+            <input
+              type="submit"
+              value="Enter"
+              className="mt-4 p-2 bg-[#000] text-[#fff] font-bold rounded w-[12rem] self-end"
+            />
+          </form>
+        </fieldset>
+      </section>
     </div>
   );
 }
