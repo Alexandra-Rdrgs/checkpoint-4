68bbda9e61e452cda67ca1b9c0a2d11cca22a192
ALEXANDRA DE_BRITO <alexandra.debrito@live.fr>
2022-07-28 15:45:19 +0200

    Add style.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -7,17 +7,23 @@ export default function About() {
       <Header />
       <article className="m-[2rem]">
         <h1 className="text-xl mb-[2rem]">About Dralanis Kingdom</h1>
-        <img src={oldCards} alt="Dralanis Kingdom" className="mb-[2rem]" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde alias
-          quos nulla modi mollitia dolore temporibus, possimus voluptatem
-          tenetur iusto corrupti, facilis veniam quae amet cumque! Totam quidem
-          ad magni voluptates. Blanditiis tempore excepturi, natus magni, nam
-          obcaecati nobis consectetur doloremque laudantium amet inventore, quod
-          repudiandae quasi minus officia repellendus! Assumenda iure labore
-          exercitationem excepturi ducimus sapiente, recusandae molestias
-          obcaecati.
-        </p>
+        <section className="flex flex-wrap gap-4">
+          <img
+            src={oldCards}
+            alt="Dralanis Kingdom"
+            className="mb-[2rem] w-[50vw]"
+          />
+          <p className="w-[40vw]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde alias
+            quos nulla modi mollitia dolore temporibus, possimus voluptatem
+            tenetur iusto corrupti, facilis veniam quae amet cumque! Totam
+            quidem ad magni voluptates. Blanditiis tempore excepturi, natus
+            magni, nam obcaecati nobis consectetur doloremque laudantium amet
+            inventore, quod repudiandae quasi minus officia repellendus!
+            Assumenda iure labore exercitationem excepturi ducimus sapiente,
+            recusandae molestias obcaecati.
+          </p>
+        </section>
       </article>
     </div>
   );
